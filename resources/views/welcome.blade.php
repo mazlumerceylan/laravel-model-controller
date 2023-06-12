4a135c5f1a165378d218a551ddf61e64604306b6
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-06-12 23:49:53 +0200

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
+    @vite(['resources/scss/style.scss'])
 </head>
 <body>
-    <div class="row">
-        @foreach ($movies as $movie)
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $movie->title }}</h5>
-                        <p class="card-text">{{ $movie->description }}</p>
-                        <a href="#" class="btn btn-primary">Vai al film</a>
+    <div class="card-container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text">{{ $movie->description }}</p>
+                            <a href="#" class="btn btn-primary">Vai al film</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 </body>
 </html>
